51f7c4d011326f74750c73d64a7003db3451f5b7
Dominik Maliszewski <dominikmaliszewski@192.168.1.186>
2025-04-02 11:28:42 +0100

    Fix: swapped App.jsx for full routing setup in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './index.css'
-import App from './App.jsx'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Competencies from './pages/Competencies'
+import Insights from './pages/Insights'
+import Consulting from './pages/Consulting'
+import Contact from './pages/Contact'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="competencies" element={<Competencies />} />
+          <Route path="insights" element={<Insights />} />
+          <Route path="consulting" element={<Consulting />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
